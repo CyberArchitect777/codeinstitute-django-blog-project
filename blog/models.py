4ddfa3d2e0240766cb837197265369355164f7b5
“CyberArchitect777” <169656116+CyberArchitect777@users.noreply.github.com>
2024-07-19 18:04:41 +0000

    Added updated_on field

--- blog/models.py
+++ blog/models.py
@@ -14,4 +14,5 @@ class Post(models.Model):
     excerpt = models.TextField(blank=True) # Is non-required
     content = models.TextField() # Blog article content
     created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
     status = models.IntegerField(choices=STATUS, default=0)
