6839579562bc0c932ab1865cd1578112d9c5cb22
“CyberArchitect777” <169656116+CyberArchitect777@users.noreply.github.com>
2024-07-19 17:03:57 +0000

    Changed the install-module.sh script to use a better approach

--- install-modules.sh
+++ install-modules.sh
@@ -1,1 +1,2 @@
-pip3 install django~=4.2.14 gunicorn~=20.1 dj-database-url~=0.5 psycopg2~=2.9
+#pip3 install django~=4.2.14 gunicorn~=20.1 dj-database-url~=0.5 psycopg2~=2.9
+pip install -r requirements.txt
