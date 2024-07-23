6b9eca848bd2bb3a940fc7562e86a1e0820847f8
“CyberArchitect777” <169656116+CyberArchitect777@users.noreply.github.com>
2024-07-23 17:44:05 +0000

    Commented urlpatterns list with useful information

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,5 @@ from django.urls import path
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
     path('<slug:slug>/', views.post_detail, name='post_detail'),
-]
+] # The second slug variable could be any name. The first is the data type.
+# as_view() not required in function based views
