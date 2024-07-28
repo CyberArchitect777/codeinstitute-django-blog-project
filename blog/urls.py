a9ee76df9bfa6ba6d8831b7f5a1633d588f5e11f
“CyberArchitect777” <169656116+CyberArchitect777@users.noreply.github.com>
2024-07-28 18:02:18 +0000

    Added edit and delete comment functionality

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,7 @@ from django.urls import path
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
     path('<slug:slug>/', views.post_detail, name='post_detail'),
+    path('<slug:slug>/edit_comment/<int:comment_id>', views.comment_edit, name='comment_edit'),
+    path('<slug:slug>/delete_comment/<int:comment_id>', views.comment_delete, name='comment_delete'),
 ] # The second slug variable could be any name. The first is the data type.
 # as_view() not required in function based views
